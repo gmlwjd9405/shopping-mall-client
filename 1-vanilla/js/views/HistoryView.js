6979feb9c358b58577fc7e41881f528981946711
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-02-19 14:00:40 +0900

    Show recent search word and date lists

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -3,25 +3,31 @@ import View from './View.js'
 const tag = '[HistoryView]'
 const HistoryView = Object.create(View)
 
+HistoryView.messages = {
+  NO_KEYWORDS: '검색 이력이 없습니다.'
+}
+
 HistoryView.setup = function (el) {
   this.init(el)
-  
+
   return this
 }
 
 HistoryView.render = function (data = []) {
-  this.el.innerHTML = data.length ? this.getHistorysHtml(data) : '최근 검색어가 없습니다.'
-  
+  this.el.innerHTML = data.length ? this.getKeywordsHtml(data) : this.messages.NO_KEYWORDS
 }
 
-HistoryView.getHistorysHtml = function (data) {
-  return data.reduce((html, item, index) => {
+HistoryView.getKeywordsHtml = function (data) {
+  return data.reduce((html, item) => {
     html += `<li data-keyword="${item.keyword}">
-    <span class="number">${index + 1}</span>
-    ${item.keyword}</li>`
+    ${item.keyword}
+    <span class="date">${item.date}</span>
+    <button class="btn-remove"></button>
+    </li>`
 
     return html
   }, '<ul class="list">') + '</ul>'
+
 }
 
 // MainController에서 HistoryView를 사용
