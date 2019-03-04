855e1dbea77957ac748e1cd7a0ae245bfa7826db
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-04 15:00:59 +0900

    [component] watch input data and add to inputValue in Formcomponent

--- 3-component/js/components/FormComponent.js
+++ 3-component/js/components/FormComponent.js
@@ -10,6 +10,13 @@ export default {
     }
   },
 
+  // 어떤 View 값을 감시하고 있다가 변경되면 처리하는 함수
+  watch: {
+    value(newVal, oldVal) {
+      this.inputValue = newVal
+    }
+  },
+
   methods: {
     onSubmit() {
       this.$emit('@submit', this.inputValue) // @submit 이벤트 발생 -> parent 이벤트로 전달 
