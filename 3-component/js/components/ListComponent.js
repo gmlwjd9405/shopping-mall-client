a42d622ad33e0f8eef8a929a9cf7cfb91c0e1f57
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-04 14:55:41 +0900

    [component] Implement listcomponent

--- 3-component/js/components/ListComponent.js
+++ 3-component/js/components/ListComponent.js
@@ -1,6 +1,15 @@
 export default {
     template: '#list',
     props: ['data', 'type'],
+    // 변수처럼 함수 사용 가능
+    computed: {
+        keywordType() {
+            return this.type === 'keywords'
+        },
+        historyType() {
+            return this.type === 'history'
+        }
+    },
     methods: {
         onClickList(keyword) {
             this.$emit('@click', keyword)
